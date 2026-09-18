172b7a61f685ca08c704fc5a91a06e843e93565c
Ali Mirzaee <107386893+alimirzae@users.noreply.github.com>
2026-09-19 00:52:21 +0330

    Move UART hardware setup to CubeMX-style MSP initialization

--- firmware/Core/Src/stm32f4xx_hal_msp.c
+++ firmware/Core/Src/stm32f4xx_hal_msp.c
@@ -36,3 +36,58 @@ void HAL_MspInit(void)
     /* SysTick_IRQn interrupt configuration */
     HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
+
+
+/**
+  * @brief UART MSP Initialization
+  *        CubeMX-style ownership of USART clocks and alternate-function GPIO.
+  */
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    if (huart->Instance == USART1)
+    {
+        __HAL_RCC_USART1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+
+        /* EWB-STM32F407 Rev2: PA9=USART1_TX, PA10=USART1_RX (AF7). */
+        GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* Polling is used at present, so USART1 NVIC is intentionally disabled. */
+    }
+    else if (huart->Instance == USART2)
+    {
+        __HAL_RCC_USART2_CLK_ENABLE();
+        __HAL_RCC_GPIOD_CLK_ENABLE();
+
+        /* RS485: PD5=USART2_TX, PD6=USART2_RX (AF7). */
+        GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+        HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+        /* Polling is used at present, so USART2 NVIC is intentionally disabled. */
+    }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+    if (huart->Instance == USART1)
+    {
+        __HAL_RCC_USART1_CLK_DISABLE();
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
+    }
+    else if (huart->Instance == USART2)
+    {
+        __HAL_RCC_USART2_CLK_DISABLE();
+        HAL_GPIO_DeInit(GPIOD, GPIO_PIN_5 | GPIO_PIN_6);
+    }
+}
