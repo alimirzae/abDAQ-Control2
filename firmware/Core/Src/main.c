da022a0f72e5b62be49d661443f93301636998fb
Ali Mirzaee <107386893+alimirzae@users.noreply.github.com>
2026-09-19 00:52:24 +0330

    Use CubeMX-style UART initialization flow

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -264,18 +264,7 @@ void SystemClock_Config(void)
 
 static void MX_USART1_UART_Init(void)
 {
-    __HAL_RCC_USART1_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /* PA9 -> USART1_TX, PA10 -> USART1_RX */
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
+    /* CubeMX pattern: peripheral GPIO/clock setup lives in HAL_UART_MspInit(). */
     huart1.Instance = USART1;
     huart1.Init.BaudRate = 115200;
     huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -294,18 +283,7 @@ static void MX_USART1_UART_Init(void)
 
 static void MX_USART2_UART_Init(void)
 {
-    __HAL_RCC_USART2_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-
-    /* PD5 -> USART2_TX, PD6 -> USART2_RX */
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
+    /* CubeMX pattern: peripheral GPIO/clock setup lives in HAL_UART_MspInit(). */
     huart2.Instance = USART2;
     huart2.Init.BaudRate = LABDAQ_MODBUS_BAUDRATE;
     huart2.Init.WordLength = UART_WORDLENGTH_8B;
